6c46f3c2b67db156ddff25a067bf36e59568c624
Mode Academy <modeacademy.github@gmail.com>
2026-05-30 12:05:02 +0300

    Initial Commit

---
+++ ModeAcademy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModeAcademy
+//
+//  Created by Burak Özdemir on 30.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
